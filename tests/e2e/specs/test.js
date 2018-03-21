d53e477976573ceebc638db090d6a851412f3821
Julien Tanay <jtanay@ooreka.fr>
2018-03-21 19:06:31 +0100

    chore: disable e2e for now with cypress

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,8 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('Test the app', () => {
-  it('Visits the Dashboard', () => {
-    cy.visit('/')
-    cy.contains('a', 'Dashboard')
-  })
-})
+// describe('Test the app', () => {
+//   it('Visits the Dashboard', () => {
+//     cy.visit('/')
+//     cy.contains('title', 'Wikiwars Helpers')
+//   })
+// })
